b215900643a48459e17b3aec9e8c8c7eb3e7f268
Gaizka <gvirum@opendeusto.es>
2017-01-17 11:39:56 +0100

    first commit

--- server.js
+++ server.js
@@ -21,12 +21,12 @@ app.use(express.static(path.join(__dirname, 'dist')));
 // Set our api routes
 app.use('/api', api);
 
-
+/*
 // Catch all other routes and return the index file
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
-
+*/
 
 /**
  * Get port from environment and store in Express.
